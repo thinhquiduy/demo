691fed665a93bf603188b733d8e850130c261bc0
DucLe98 <32091575+DucLe98@users.noreply.github.com>
2017-12-11 22:35:14 +0700

    Của Đức nè

---
+++ CBBplDGV/CBBplDGV/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+
+namespace CBBplDGV
+{
+    public partial class Form1 : Form
+    {
+        static string conString = "Provider=SQLNCLI11;Data Source=DESKTOP-PM1OOCL;Integrated Security=SSPI;Initial Catalog=FootballPlayer";
+        DataTable dt = new DataTable();
+        OleDbConnection con = new OleDbConnection(conString);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillcombo();
+            dataGridView1.DataSource = loadData();
+        }
+
+        private DataTable loadData()
+        {
+            OleDbDataAdapter adapter = new OleDbDataAdapter();
+            OleDbCommand cmd;
+            DataSet ds = new DataSet();
+
+            string sql = "SELECT * FROM VITRI";
+            cmd = new OleDbCommand(sql, con);
+
+            adapter.SelectCommand = cmd;
+            adapter.Fill(ds);
+
+            dt = ds.Tables[0];
+
+            return dt;
+        }
+
+        private void fillcombo()
+        {
+            //them cot combobox
+            DataGridViewComboBoxColumn combo = new DataGridViewComboBoxColumn();
+            combo.HeaderText = "Dong Doi";
+            combo.Name = "combo";
+            ArrayList row = new ArrayList();
+            
+            //them items cho combo tu datatable
+            foreach(DataRow dr in dt.Rows)
+            {
+                row.Add(dr["Ten"].ToString());
+            }
+
+            //them vao combo
+            combo.Items.AddRange(row.ToArray());
+
+            //them vao combo column
+            dataGridView1.Columns.Add(combo);
+        }
+    }
+}
---
+++ DoanhTh/DoanhTh/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DoanhTh
+{
+    public partial class Form1 : Form
+    {
+        DataTable dsDoanhThu = new DataTable();
+        DataView dsDoanhThuView;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        public  DataTable docBang()
+        {
+            DataTable dt = new DataTable();
+            OleDbDataAdapter adapter = new OleDbDataAdapter("Select * From DoanhThu", "Provider=SQLNCLI11;Data Source=DESKTOP-PM1OOCL;Integrated Security=SSPI;Initial Catalog=DOANHTHU");
+            adapter.Fill(dt);
+            return dt;
+        }
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            dsDoanhThu = docBang();
+            //dsDoanhThuView = new DataView(dsDoanhThu);
+            dgvdsDoanhThu.DataSource = dsDoanhThu;
+            dataGridView1.DataSource = dsDoanhThu;
+            dataGridView2.DataSource = dsDoanhThu;
+            dataGridView3.DataSource = dsDoanhThu;
+        }
+
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }   
+
+
+        /*public string Get_Day()
+        {
+            string str = DateTime.Now.ToString().Trim();
+            str = str.Substring(0, 2);
+            return str;
+        }
+        public string Get_Month()
+        {
+            string str = DateTime.Now.ToString().Trim();
+            str = str.Substring(3, 2);
+            return str;
+        }
+        public string Get_Year()
+        {
+            string str = DateTime.Now.ToString().Trim();
+            str = str.Substring(6, 4);
+            return str;
+        }
+
+        private void thang_TextChanged(object sender, EventArgs e)
+        {
+            thang.Text = Get_Month();
+        }
+
+
+        private void ngay_TextChanged_1(object sender, EventArgs e)
+        {
+            ngay.Text = Get_Day();
+        }
+
+        private void nam_TextChanged(object sender, EventArgs e)
+        {
+            nam.Text = Get_Year();
+        }*/
+    }
+}
